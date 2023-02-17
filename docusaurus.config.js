afff9fbb835c3789eceb698b78994d635ce6b5a6
Michael Marshall <michaelmarshall220@gmail.com>
2023-02-17 11:00:36 -0600

    updated config to not throw on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "Accelerating Bible Translation",
   url: "https://dev.lab.eten.bible",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
